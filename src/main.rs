e1f6aac7df832efced3e1842fdf9d1f049a0e381
Kenneth Love <klove@oreilly.com>
2024-04-01 15:15:33 -0700

    GLTF scene displays

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use bevy::{asset::AssetMetaCheck, prelude::*};
+use std::f32::consts::FRAC_PI_2;
+
+use bevy::{asset::AssetMetaCheck, prelude::*, render::camera::ScalingMode};
 use bevy_embedded_assets::{EmbeddedAssetPlugin, PluginMode};
 use bevy_kira_audio::AudioPlugin;
 
@@ -27,5 +29,62 @@ fn main() {
         //     RapierDebugRenderPlugin::default(),
         // ))
         .add_systems(Update, bevy::window::close_on_esc)
+        .add_systems(Startup, setup)
         .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    ass: Res<AssetServer>,
+) {
+    // circular base
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Circle::new(4.)),
+        material: materials.add(Color::WHITE),
+        transform: Transform::from_rotation(Quat::from_rotation_x(-FRAC_PI_2)),
+        ..default()
+    });
+
+    // cube
+    // commands.spawn(PbrBundle {
+    //     mesh: meshes.add(Cuboid::new(1., 1., 1.)),
+    //     material: materials.add(Color::rgb_u8(124, 144, 255)),
+    //     transform: Transform::from_xyz(0., 0.5, 0.),
+    //     ..default()
+    // });
+
+    let transform = Transform {
+        translation: Vec3::new(0., 0., 0.),
+        rotation: Quat::from_rotation_x(-FRAC_PI_2),
+        ..default()
+    };
+
+    let my_gltf = ass.load("LowPolychars3.glb#Scene0");
+    commands.spawn(SceneBundle {
+        scene: my_gltf,
+        transform,
+        ..default()
+    });
+
+    // light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4., 8., 4.),
+        ..default()
+    });
+
+    // camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(2.5, 3., 4.).looking_at(Vec3::ZERO, Vec3::Y),
+        projection: OrthographicProjection {
+            scaling_mode: ScalingMode::FixedVertical(6.),
+            ..default()
+        }.into(),
+        ..default()
+    });
 }
